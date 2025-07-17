a4bf0a28ece398f6e88f8591c6e61e19a651e4ec
Saad El-Kenawy <Saad_elkenawy@yahoo.com>
2025-07-18 02:55:25 +0300

    remove line

---
+++ hello.sh
@@ -0,0 +1,9 @@
+#welcome to jenkins whook
+#!/bin/bash
+echo "Hello, Jenkins! This is a webhook test."
+# This script is intended to be triggered by a webhook event in Jenkins.
+# It can be used to perform actions such as notifying a team, triggering builds, or logging events.
+# You can customize this script to perform any actions you need in response to the webhook event.
+hostname=$(hostname)
+echo "This script is running on host: $hostname" 
+# You can add more commands here to perform additional tasks.
