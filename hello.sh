5314726d334a4a845ed5519af1e41d2b4a6656cf
Saad El-Kenawy <Saad_elkenawy@yahoo.com>
2025-07-18 05:23:27 +0300

    add hostname and  name and family

--- hello.sh
+++ hello.sh
@@ -11,5 +11,5 @@ echo "This script was executed by: $name"
 famil_name = "El-Kenawy"
 echo "This script was executed by: $famil_name"
 echo "This script is running on host: $hostname and was executed by $name $famil_name"
-
+echo "This script is running on host: $hostname and was executed by $name $famil_name"
 # End of the script
